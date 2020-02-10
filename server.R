2f82543dcae10a265506bf953bac7eeaf4282d6d
Matthew Lincoln <matthew.d.lincoln@gmail.com>
2020-02-10 16:26:18 -0500

    build out basic tf-idf table and bookworm chart

--- server.R
+++ server.R
@@ -1,8 +1,9 @@
 library(shiny)
 library(tidytext)
 library(glue)
+library(lubridate)
 
-function(input, output) {
+function(input, output, session) {
 
   available_corpora <- reactive({
     collect(corpora)
@@ -12,8 +13,41 @@ function(input, output) {
     return(st$get("corpus-1-tidy"))
   })
 
-  corpus_tfidf <- reactive({
+  corpus_metadata <- reactive({
+    corpus_document %>%
+      filter(corpus_id == 1) %>%
+      inner_join(document_metadata, by = "document_id") %>%
+      arrange(date, parent_title, item_title) %>%
+      collect()
+  })
+
+  token_choices <- reactive({
+    toks <- corpus_tokens() %>%
+      distinct(gram) %>%
+      pull(gram)
+  })
+
+  # Update token selectize menus based on selected corpora tokens
+  observe({
+    x <- token_choices()
+    updateSelectizeInput(session, "tfidf_stoplist",
+                         choices = x,
+                         server = TRUE)
+    updateSelectizeInput(session, "wordchart_tokens",
+                         choices = x,
+                         selected = c("regulation", "big data"),
+                         server = TRUE)
+  })
+
+  # TF-IDF ----
+
+  refined_tf_idf_tokens <- reactive({
     corpus_tokens() %>%
+      filter(!(gram %in% input$tfidf_stoplist))
+  })
+
+  corpus_tfidf <- reactive({
+    refined_tf_idf_tokens() %>%
       bind_tf_idf(gram, document_id, n) %>%
       group_by(document_id) %>%
       filter(row_number(desc(tf_idf)) <= 10) %>%
@@ -21,25 +55,58 @@ function(input, output) {
       summarize(top_terms = str_c(gram, collapse = ", "))
   })
 
-  corpus_decade_tfidf <- reactive({
-    ethics_and_ai_tokens %>%
-      left_join(document_metadata %>% filter(language == "eng") %>% select(document_id, year), by = "document_id", copy = TRUE) %>%
-      group_by(gram) %>%
-      filter(sum(n) > 10) %>%
-      group_by(year, gram) %>%
-      summarize(nn = sum(n)) %>%
-      bind_tf_idf(gram, year, nn) %>%
-      group_by(year) %>%
-      filter(row_number(desc(tf_idf)) <= 10) %>%
-      arrange(desc(tf_idf)) %>%
-      summarize(top_terms = str_c(gram, collapse = ", "))
-  })
+  # corpus_decade_tfidf <- reactive({
+  #   ethics_and_ai_tokens %>%
+  #     left_join(document_metadata %>% filter(language == "eng") %>% select(document_id, year), by = "document_id", copy = TRUE) %>%
+  #     group_by(gram) %>%
+  #     filter(sum(n) > 10) %>%
+  #     group_by(year, gram) %>%
+  #     summarize(nn = sum(n)) %>%
+  #     bind_tf_idf(gram, year, nn) %>%
+  #     group_by(year) %>%
+  #     filter(row_number(desc(tf_idf)) <= 10) %>%
+  #     arrange(desc(tf_idf)) %>%
+  #     summarize(top_terms = str_c(gram, collapse = ", "))
+  # })
 
   output$document_metadata <- renderDataTable({
     corpus_tfidf() %>%
-      inner_join(document_metadata, by = "document_id", copy = TRUE) %>%
-      select(item_title, parent_title, date, url, top_terms) %>%
-      collect() %>%
-      mutate(url = glue("<a href={url}>{url}</a>"))
+      inner_join(corpus_metadata(), by = "document_id") %>%
+      select(doc_type, item_title, parent_title, date, url, top_terms) %>%
+      mutate(
+        date = ymd(date),
+        url = glue("<a href={url}>{url}</a>")
+        )
   }, escape = FALSE)
+
+  # Bookworm ----
+
+  bookworm_tokens <- reactive({
+    corpus_document %>%
+      filter(corpus_id == 1) %>%
+      left_join(document_metadata, by = "document_id") %>%
+      select(document_id, year) %>%
+      collect()
+    })
+
+  bookworm_data <- reactive({
+    corpus_tokens() %>%
+      left_join(bookworm_tokens(), by = "document_id") %>%
+      group_by(year) %>%
+      mutate(total_docs = n_distinct(document_id)) %>%
+      ungroup() %>%
+      filter(gram %in% input$wordchart_tokens) %>%
+      group_by(year, gram) %>%
+      summarize(
+        percent_total = n() / first(total_docs)
+      )
+  })
+
+  output$bookworm_chart <- renderPlot({
+    bookworm_data() %>%
+      ggplot(aes(x = year, y = percent_total)) +
+      geom_line(aes(color = gram)) +
+      xlim(2014, 2020) +
+      theme_minimal()
+  })
 }
--- ui.R
+++ ui.R
@@ -1,20 +1,51 @@
-library(shiny)
+library(shinydashboard)
 
 # Define UI for application that draws a histogram
-fluidPage(
-    # Application title
-    titlePanel("Corpus TF-IDF"),
+dash_header <- dashboardHeader(
+    title = "Exploravec(tor)"
+)
 
-    # Sidebar with a slider input for number of bins
-    sidebarLayout(
-        sidebarPanel(
+corpus_tf_idf_tab <- tabItem(
+    tabName = "corpus_tf_idf",
+    h2("TF-IDF of the corpus documents"),
+    box(
+        selectizeInput("tfidf_stoplist", choices = NULL, selected = NULL, multiple = TRUE, label = "Eliminate tokens from TF-IDF"),
+        p("Add tokens to exluce from the TF-IDF calculations.")
+    ),
+    box(dataTableOutput("document_metadata"),
+        title = "Document TF-IDF",
+        width = 12)
+)
 
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-            dataTableOutput("document_metadata")
-        )
+bookworm_tab <- tabItem(
+    tabName = "bookworm",
+    h2("Terms over time"),
+    box(
+        selectizeInput("wordchart_tokens", choices = NULL, selected = NULL, multiple = TRUE, label = "Pick tokens to plot over time")
+    ),
+    box(
+        plotOutput("bookworm_chart"),
+        width = 12
     )
 )
 
+dash_sidebar <- dashboardSidebar(
+    sidebarMenu(
+        menuItem("Bookworm", tabName = "bookworm", icon = icon("chart-line")),
+        menuItem("TF-IDF", tabName = "corpus_tf_idf", icon = icon("sort-amount-down"))
+    )
+)
+
+dash_body <- dashboardBody(
+    tabItems(
+        bookworm_tab,
+        corpus_tf_idf_tab
+    )
+)
+
+dashboardPage(
+    dash_header,
+    dash_sidebar,
+    dash_body,
+    title = "AI and Ethics Corpus Explorer"
+)
